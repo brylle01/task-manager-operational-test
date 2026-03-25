7ffc4ced41b5e88e20ed0e146d4a3ddb1100828f
brylle01 <elbryllepagasian736@gmail.com>
2026-03-26 04:54:05 +0800

    order task by status and created_at

--- backend/task/models.py
+++ backend/task/models.py
@@ -8,5 +8,8 @@ class Task(models.Model):
   completed = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  class Meta:
+    ordering = ['completed', '-created_at']
+
   def __str__(self):
     return self.title
